1de8f62171c939384818856e594a04512ecfd46f
gbatistela <giulianobatistela@gmail.com>
2024-12-05 14:41:27 -0300

    FIrst

--- react-frontend/src/components/Header.jsx
+++ react-frontend/src/components/Header.jsx
@@ -1,32 +1,41 @@
-import React, { useState } from 'react';
-import './header.css';
+import React, { useState } from "react";
+import "./Header.css";
 
 const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <header className="header">
-            <div className="logo"><img src="logorestaurante.jpg" alt="Logo" /></div>
-            <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">Menu</a></li>
-                    <li><a href="#services">Contacto</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>
-            <div className="menu-toggle" onClick={toggleMenu}>
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
-            </div>
-        </header>
-    );
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logorestaurante.jpg" alt="Logo" />
+      </div>
+      <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">Menu</a>
+          </li>
+          <li>
+            <a href="#services">Contacto</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
-
